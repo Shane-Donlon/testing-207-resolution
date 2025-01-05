0fa5252778bcfa370d9c35a4db93cd819cc825e6
Shane-Donlon <shane.donlon@live.com>
2025-01-05 08:28:16 +0000

    testing netlify adapter

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,11 @@
 import qwikdev from "@qwikdev/astro";
 import { defineConfig } from "astro/config";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [qwikdev()],
+  output: 'server',
+  adapter: netlify(),
 });
